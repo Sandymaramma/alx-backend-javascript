46cf8e9747be0da09a4fc4b4a70d353cde53ac69
sandymaramma <edesandraoge@gmail.com>
2023-07-27 02:25:23 +0100

    update

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,5 @@
+
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-  ],
-};module.exports = {
   presets: [
     [
       '@babel/preset-env',
