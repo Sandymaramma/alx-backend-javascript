558975f8ed3d1dd323af86dcc6580e7c3b74c6e5
sandymaramma <edesandraoge@gmail.com>
2023-07-27 03:00:11 +0100

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-// create a Promise object
 export default function getResponseFromAPI() {
-  const promise = new Promise((resolve, reject) => {});
-  return promise;
+  return new Promise(() => {});
+}
