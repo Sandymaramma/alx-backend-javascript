3c6d352c91d468abd470f559c4a8c2b78187cba3
sandymaramma <edesandraoge@gmail.com>
2023-07-28 15:40:23 +0100

    100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,23 +1,13 @@
+/* eslint-disable class-methods-use-this */
 import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this.range = range;
-  }
-
-  get range() {
-    return this._range;
-  }
-
-  set range(value) {
-    this._range = value;
+    this._range = range;
   }
 
   cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
+    return new Car();
   }
 }
-
