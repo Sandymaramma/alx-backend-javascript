973c8c8804afa6ca7cbb64b9bf9a40fc8b60d0fe
sandymaramma <edesandraoge@gmail.com>
2023-07-28 14:56:06 +0100

    0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,14 +1,6 @@
-/**
- * Represents a class room.
- */
+/* eslint-disable no-underscore-dangle */
 export default class ClassRoom {
-  /**
-   * Creates a new @see {@link ClassRoom}.
-   * @param {Number} maxStudentsSize - The maximum number of
-   * students in the class.
-   */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
 }
-
