edc8aae441878df31358268dc2185f96071f584c
sandymaramma <edesandraoge@gmail.com>
2023-07-28 14:30:29 +0100

    9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
-export default function guardrail(mathFunction) {
+export default function gaurdrail(myFunction) {
   const queue = [];
   try {
-    queue.push(mathFunction());
-  } catch (error) {
-    queue.push('Error: ${error.message}`);
+    const stuff = myFunction();
+    queue.push(stuff);
+  } catch (err) {
+    queue.push(`Error: ${err.message}`);
   } finally {
-  queue.push('Guardrail was processed');
+    queue.push('Guardrail was processed');
   }
   return queue;
 }
-
