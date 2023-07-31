64d11975caaccbe39465bc6a43310c6bf2f2911e
sandymaramma <edesandraoge@gmail.com>
2023-07-31 22:33:51 +0100

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,14 @@
+// Cresting a function named getListStudents, returns an array of objects
+// Each object should have three attributes: id (Number), firstName (String), and location (String).
+
+/* eslint-disabled */
+const getListStudents =() => {
+  const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const student3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  const newarr = [];
+  newarr.push(student1, student2, student3);
+  return newarr;
+};
+
+export default getListStudents;
