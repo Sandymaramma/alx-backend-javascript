0d2cec684363d121a3b60c2b88f65e3ac3fbdb6e
sandymaramma <edesandraoge@gmail.com>
2023-08-01 10:20:11 +0100

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,11 @@
-export default function updateStudentGradeByCity(stufents, city, newGrades) {
-	return students
-	.filter((student student.location.localeCompare(city) === 0)
-	.map((a) => {
-		const newGrade = newGrade.filter(b) => b.studentId === a.id);
-		const student = a;
-		lf (newGrade.length === 1) student.grade = newGrade[0].grade;
-		else student.grade = 'N/A';
-		return student;
-	});
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location.localeCompare(city) === 0)
+    .map((x) => {
+      const newGrade = newGrades.filter((y) => y.studentId === x.id);
+      const student = x;
+      if (newGrade.length === 1) student.grade = newGrade[0].grade;
+      else student.grade = 'N/A';
+      return student;
+    });
 }
